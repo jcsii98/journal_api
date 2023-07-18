cc9518b7d7d3aa0f0de25dfc7f01f0448a776910
Jose Saribong <jose.c.saribong@gmail.com>
2023-07-18 23:06:16 +0800

    CORS origin config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "https://journal-app-t8n9.onrender.com/"
+    origins "http://localhost:5173", "https://journal-app-t8n9.onrender.com"
 
     resource "*",
       headers: :any,
